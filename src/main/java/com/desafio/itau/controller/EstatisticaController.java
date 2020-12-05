a28f7af87337528bc513378c98f2d846320432cc
Jefferson Russo Silva <jefferson.russo@Outlook.com>
2020-12-05 15:14:35 -0300

    modificacao na classe EstatisticaController

--- src/main/java/com/desafio/itau/controller/EstatisticaController.java
+++ src/main/java/com/desafio/itau/controller/EstatisticaController.java
@@ -3,11 +3,12 @@ package com.desafio.itau.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.desafio.itau.model.Estatistica;
-import com.desafio.itau.model.Transacao;
 import com.desafio.itau.repository.TransacaoRepository;
 import com.desafio.itau.service.CalEstatisticaService;
 
@@ -28,4 +29,10 @@ public class EstatisticaController {
 		
 		return ResponseEntity.ok(calEst.getEstatistica());
 	}
+	
+	@PostMapping ("/intervalo/{milisegundos}")
+	public void post(@PathVariable long milisegundos) {
+		calEst.setIntervaloEmMilesegundos(milisegundos);
+	}
+
 }
